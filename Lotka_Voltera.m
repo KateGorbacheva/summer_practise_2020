1a29f800a4193c967204fedfaca83b99bb01c0af
KateGorbacheva <61273366+KateGorbacheva@users.noreply.github.com>
2020-07-15 12:23:00 +0300

    Create Lotka_Voltera.m

---
+++ Lotka_Voltera.m
@@ -0,0 +1,47 @@
+function Lotka_Voltera(x1,x2,X_2,B,alpha1,alpha2,beta1,beta2,w1)
+%{
+x1  - начальное значение жертв
+x2  - начальное значение хищников
+X_2 - цель хищников
+B   - ограничение жертв
+alpha1,alpha2,beta1,beta2,w1 - коэффициенты
+%}
+n0 = 1;%левая граница
+nn = 50;%правая граница
+h = 0.01;%шаг
+interval = n0:h:nn;
+
+N = length(interval);
+
+X1 = zeros(1,N);
+X2 = zeros(1,N);
+X1(1)=x1;
+X2(1)=x2;
+
+
+for n=2:N
+psi=X1(n-1)+ B*tanh(X2(n-1) - X_2);
+P=(4*exp(2*psi))/((exp(2*psi)+1)^2);
+u = -w1^(-1)*psi -(1+B*P)*(alpha1*X1(n-1) - beta1*X1(n-1)*X2(n-1));%+B*P*(X_2); 
+    
+X1(n)=X1(n-1) + h*(alpha1*X1(n-1) - beta1*X1(n-1)*X2(n-1)+u);
+X2(n)=X2(n-1) + h*(-alpha2*X2(n-1)+beta2*X1(n-1)*X2(n-1));
+
+
+end
+
+Xvals = 1:N;
+
+hold on;
+plot(Xvals,X1,'b','LineWidth',1);
+plot(Xvals,X2,'--','Color',[.1 .7 .7],'LineWidth',1);
+set(0,'DefaultAxesFontSize',14,'DefaultAxesFontName','Times New Roman');
+set(0,'DefaultTextFontSize',14,'DefaultAxesFontName','Times New Roman');
+legend('Жертва', 'Хищник');
+title('Хищник - Жертва');
+grid on;
+ylabel('X1,X2');
+xlabel('time');
+hold off;
+
+end
